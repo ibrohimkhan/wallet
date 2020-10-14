d851c1a1326ce777bbcf46278ee839b73472a816
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-10-15 03:13:47 +0500

    added export/import feature

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,123 @@
 package main
 
 import (
+	"path/filepath"
+	"log"
+	"os"
 	"fmt"
 	"github.com/ibrohimkhan/wallet/v1.1.0/pkg/wallet"
 )
 
 func main() {
+	importData()
+	//exportData()
+}
+
+func importData() {
+	s := &wallet.Service{}
+
+	path, err := filepath.Abs(".")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fullpath := path + "/data"
+	log.Println(fullpath)
+
+	err = s.Import(fullpath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func exportData() {
+	s := &wallet.Service{}
+
+	createData(s)
+
+	path, err := filepath.Abs(".")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fullpath := path + "/data"
+	log.Println(fullpath)
+
+	err = s.Export(fullpath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func createData(s *wallet.Service) {
+	account, err := s.RegisterAccount("+992937452945")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	s.Deposit(account.ID, 100)
+	s.Pay(account.ID, 15, "auto")
+	payment, err := s.Pay(account.ID, 25, "auto")
+	s.FavoritePayment(payment.ID, "auto")
+
+	account, err = s.RegisterAccount("+992937452946")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	s.Deposit(account.ID, 101)
+	s.Pay(account.ID, 13, "auto")
+	payment, err = s.Pay(account.ID, 77, "book")
+	s.FavoritePayment(payment.ID, "auto")
+
+	account, err = s.RegisterAccount("+992937452947")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	s.Deposit(account.ID, 102)
+	s.Pay(account.ID, 35, "book")
+	s.Pay(account.ID, 33, "book")
+}
+
+func path() {
+	abs, err := filepath.Abs(".")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(abs)
+
+	path, err := filepath.Abs(abs)
+	path += "/accounts.dump"
+	log.Println(path)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(wd)
+
+	err = os.Chdir("..")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	wd, err = os.Getwd()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(wd)		
+}
+
+func registerAccount() {
 	svc := &wallet.Service{}
 	account, err := svc.RegisterAccount("+992937452945")
 	if err != nil {
--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"path/filepath"
 	"strings"
 	"io"
 	"strconv"
@@ -194,6 +195,168 @@ func (s *Service) FindFavoriteByID(favoriteID string) (*types.Favorite, error) {
 
 // ExportToFile saves accounts into a file
 func (s *Service) ExportToFile(path string) error {
+	err := s.exportAccountsToFile(path)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// ImportFromFile restores accounts into objects
+func (s *Service) ImportFromFile(path string) error {
+	data, err := s.getDataFromFile(path)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	accounts := s.parseStringToAccounts(data)
+	for _, account := range accounts {
+		s.accounts = append(s.accounts, account)
+	}
+
+	return nil
+}
+
+// Export all available data (accounts, payments and favorites) to the given dir in files
+func (s *Service) Export(dir string) error {
+	if s.accounts != nil && len(s.accounts) > 0 {
+		fullpath, err := s.getFullPath(dir, "accounts.dump")
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		err = s.exportAccountsToFile(fullpath) 
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	if s.payments != nil && len(s.payments) > 0 {
+		fullpath, err := s.getFullPath(dir, "payments.dump")
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		
+		err = s.exportPaymentsToFile(fullpath)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	if s.favorites != nil && len(s.favorites) > 0 {
+		fullpath, err := s.getFullPath(dir, "favorites.dump")
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		
+		err = s.exportFavoritesToFile(fullpath)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Import all data from the given dir into objects such as accounts, payments and favorites
+func (s *Service) Import(dir string) error {
+	path, err := filepath.Abs(dir)
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+
+	accountsPath := path + "/accounts.dump"
+	accounts, err := s.importAccountsFromFile(accountsPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, dumpAccount := range accounts {
+		for _, inmemoryAccount := range s.accounts {
+			if dumpAccount.ID == inmemoryAccount.ID {
+				inmemoryAccount.Phone = dumpAccount.Phone
+				inmemoryAccount.Balance += dumpAccount.Balance
+			} else {
+				s.accounts = append(s.accounts, dumpAccount)
+				s.nextAccountID++
+			}
+		}
+	}
+
+	paymentsPath := path + "/payments.dump"
+	payments, err := s.importPaymentsFromFile(paymentsPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, dumpPayment := range payments {
+		for _, inmemoryPayment := range s.payments {
+			if dumpPayment.ID == inmemoryPayment.ID {
+				inmemoryPayment.AccountID 	= dumpPayment.AccountID
+				inmemoryPayment.Amount 		= dumpPayment.Amount
+				inmemoryPayment.Category 	= dumpPayment.Category
+				inmemoryPayment.Status 		= dumpPayment.Status
+			} else {
+				s.payments = append(s.payments, dumpPayment)
+			}
+		}
+	}
+
+	favoritesPath := path + "/favorites.dump"
+	favorites, err := s.importFavoritesFromFile(favoritesPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, dumpFavorite := range favorites {
+		for _, inmemoryFavorite := range s.favorites {
+			if dumpFavorite.ID == inmemoryFavorite.ID {
+				inmemoryFavorite.AccountID 	= dumpFavorite.AccountID
+				inmemoryFavorite.Name 		= dumpFavorite.Name
+				inmemoryFavorite.Amount 	= dumpFavorite.Amount
+				inmemoryFavorite.Category 	= dumpFavorite.Category
+			} else {
+				s.favorites = append(s.favorites, dumpFavorite)
+			}
+		}
+	}
+
+	log.Println("size of accounts = ", len(accounts))
+	log.Println("size of payments = ", len(payments))
+	log.Println("size of favorites = ", len(favorites))
+
+	return nil
+}
+
+func (s *Service) getFullPath(dir string, filename string) (string, error) {
+	path, err := filepath.Abs(dir)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, 0700)
+	} 
+	
+	fullpath := path + "/" + filename
+	return fullpath, nil
+}
+
+func (s *Service) exportAccountsToFile(path string) error {
 	file, err := os.Create(path)
 	if err != nil {
 		log.Println(err)
@@ -218,12 +381,91 @@ func (s *Service) ExportToFile(path string) error {
 	return nil
 }
 
-// ImportFromFile restores accounts into objects
-func (s *Service) ImportFromFile(path string) error {
-	file, err := os.Open(path)
+func (s *Service) exportPaymentsToFile(path string) error {
+	file, err := os.Create(path)
 	if err != nil {
 		log.Println(err)
 		return err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	for _, payment := range s.payments {
+		parsed := s.parsePaymentToString(payment)
+		_, err := file.Write([]byte(parsed))
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) exportFavoritesToFile(path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	for _, favorite := range s.favorites {
+		parsed := s.parseFavoriteToString(favorite)
+		_, err := file.Write([]byte(parsed))
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) importAccountsFromFile(path string) ([]*types.Account, error) {
+	data, err := s.getDataFromFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := s.parseStringToAccounts(data)
+	return accounts, nil
+}
+
+func (s *Service) importPaymentsFromFile(path string) ([]*types.Payment, error) {
+	data, err := s.getDataFromFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	payments := s.parseStringToPayments(data)
+	return payments, nil
+}
+
+func (s *Service) importFavoritesFromFile(path string) ([]*types.Favorite, error) {
+	data, err := s.getDataFromFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	favorites := s.parseStringToFavorites(data)
+	return favorites, nil
+}
+
+func (s *Service) getDataFromFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return "", err
 	}
 
 	defer func() {
@@ -243,26 +485,20 @@ func (s *Service) ImportFromFile(path string) error {
 
 		if err != nil {
 			log.Println(err)
-			return err
+			return "", err
 		}
 
 		content = append(content, buf[:read]...)
 	}
 
 	data := string(content)
-	accounts := s.parseStringToAccounts(data)
-
-	for _, account := range accounts {
-		s.accounts = append(s.accounts, account)
-	}
-	
-	return nil
+	return data, nil
 }
 
 func (s *Service) parseAccountToString(account *types.Account) string {
 	parsed := strconv.FormatInt(account.ID, 10) + ";"
 	parsed += string(account.Phone) + ";"
-	parsed += strconv.FormatInt(int64(account.Balance), 10) + "|"
+	parsed += strconv.FormatInt(int64(account.Balance), 10) + "\n"
 	
 	return parsed
 }
@@ -270,7 +506,8 @@ func (s *Service) parseAccountToString(account *types.Account) string {
 func (s *Service) parseStringToAccounts(data string) []*types.Account {
 	var accounts []*types.Account
 
-	for _, items := range strings.Split(data, "|") {
+	data = strings.TrimSpace(data)
+	for _, items := range strings.Split(data, "\n") {
 		item := strings.Split(items, ";")
 
 		if len(item) < 3 {
@@ -298,4 +535,78 @@ func (s *Service) parseStringToAccounts(data string) []*types.Account {
 	}
 
 	return accounts
+}
+
+func (s *Service) parsePaymentToString(payment *types.Payment) string {
+	parsed := payment.ID + ";"
+	parsed += strconv.FormatInt(payment.AccountID, 10) + ";"
+	parsed += strconv.FormatInt(int64(payment.Amount), 10) + ";"
+	parsed += string(payment.Category) + ";"
+	parsed += string(payment.Status) + "\n"
+
+	return parsed
+}
+
+func (s *Service) parseStringToPayments(data string) []*types.Payment {
+	var payments []*types.Payment
+
+	data = strings.TrimSpace(data)
+	for _, items := range strings.Split(data, "\n") {
+		item := strings.Split(items, ";")
+
+		id			 	:= string(item[0])
+		accountID, _ 	:= strconv.ParseInt(item[1], 10, 64)
+		amount, _ 		:= strconv.ParseInt(item[2], 10, 64)
+		category		:= string(item[3])
+		status			:= string(item[4])
+
+		payment := &types.Payment {
+			ID:				id,
+			AccountID:		accountID,
+			Amount:			types.Money(amount),
+			Category:		types.PaymentCategory(category),
+			Status:			types.PaymentStatus(status),
+		}
+
+		payments = append(payments, payment)
+	}
+
+	return payments
+}
+
+func (s *Service) parseFavoriteToString(favorite *types.Favorite) string {
+	parsed := favorite.ID + ";"
+	parsed += strconv.FormatInt(favorite.AccountID, 10) + ";"
+	parsed += favorite.Name + ";"
+	parsed += strconv.FormatInt(int64(favorite.Amount), 10) + ";"
+	parsed += string(favorite.Category) + "\n"
+
+	return parsed
+}
+
+func (s *Service) parseStringToFavorites(data string) []*types.Favorite {
+	var favorites []*types.Favorite
+
+	data = strings.TrimSpace(data)
+	for _, items := range strings.Split(data, "\n") {
+		item := strings.Split(items, ";")
+
+		id			 	:= string(item[0])
+		accountID, _ 	:= strconv.ParseInt(item[1], 10, 64)
+		name			:= string(item[2])
+		amount, _ 		:= strconv.ParseInt(item[3], 10, 64)
+		category		:= string(item[4])
+
+		favorite := &types.Favorite {
+			ID:				id,
+			AccountID:		accountID,
+			Name:			name,
+			Amount:			types.Money(amount),
+			Category:		types.PaymentCategory(category),
+		}
+
+		favorites = append(favorites, favorite)
+	}
+
+	return favorites
 }
